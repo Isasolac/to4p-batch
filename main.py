81d741146e82b76413c3638b5c88cee3a6fb102a
Isabel Gardner <igardner@andrew.cmu.edu>
2023-04-13 13:49:21 -0400

    merging current changes

--- main.py
+++ main.py
@@ -18,6 +18,50 @@ def main():
 
     for image in args.images:
         print(image)
+
+        # Step 1: use mmls to find filesystems
+        stream = os.popen('mmls ./'+image)
+        output = stream.read()
+        print(output)
+
+        filesystem_lines = []
+        useful_data = False
+
+        for line in output.splitlines():
+            if useful_data:
+                parse_mmls_line(line)
+            if "Slot" in line:
+                useful_data = True
+
+'''
+count: the number ID of the resulting carved filesystem ex "1_1"
+'''
+def cat_slot(slot_num, image_name, count):
+    pass
+
+'''
+Returns a tuple with: (slot number, is_partition, start, end, length, description)
+'''
+def parse_mmls_line(line):
+
+    slot_partition = line.partition(":")
+    # WARNING = could split at more than one area
+
+    slot_num = slot_partition[0]
+
+    # split the rest into tokens using split
+    tokens = line.split()
+
+    description = tokens[5]
+    
+    if tokens[1] != "-----":
+        is_partition = True
+
+    
+
+
+
+    return(slot_num, is_partition,)
     
 
 def ntfs_parse(ntfs):
