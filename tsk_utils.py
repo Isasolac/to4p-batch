e0e71b7126353f4a24f02666b929492a3ca5cf12
Ankshit Jain <ankshitj@andrew.cmu.edu>
2023-04-20 15:13:36 -0400

    Added parsing related files

---
+++ tsk_utils.py
@@ -0,0 +1,37 @@
+import os 
+import shlex
+import subprocess
+
+from fs import SupportedTypes
+
+def check_if_file_exists(path):
+    if not os.path.isfile(path):
+        raise FileNotFoundError("Could not find disk image!")
+
+def run_command(command):
+    process = subprocess.run(shlex.split(command), text=True)
+    return process.returncode, process.stdout, process.stderr
+
+def mmls(dd_image_path):
+    check_if_file_exists(dd_image_path)
+    
+
+def fsstat(dd_image_path):
+    check_if_file_exists(dd_image_path)
+    returncode, stdout, stderr = run_command('fsstat ' + dd_image_path)
+    return returncode, stdout, stderr
+
+def get_fs_type(dd_image_path):
+    returncode, stdout, _ = fsstat(dd_image_path)
+    lines = [line.strip() for line in stdout.split("\n")]
+    if returncode == 0:
+        for line in lines:
+            if line.startswith("File System Type: "):
+                if line.endswith("FAT16"):
+                    return (SupportedTypes.FAT16, lines)
+                elif line.endswith("FAT32"):
+                    return (SupportedTypes.FAT32, lines)
+                elif line.endswith("NTFS"):
+                    return (SupportedTypes.NTFS, lines)
+                
+    return (SupportedTypes.Unsupported, None)
