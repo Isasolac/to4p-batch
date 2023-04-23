1e8c7558b19853611278d21db7d9d2757d73266d
Ankshit Jain <ankshitjain@gmail.com>
2023-04-23 13:09:10 -0700

    Added fiwalk (#8)

--- tsk_utils.py
+++ tsk_utils.py
@@ -1,8 +1,9 @@
 import os 
-import shlex
 import subprocess
-
+from xml.etree import ElementTree
 from fs import SupportedTypes
+
+FIWALK_DEFAULT_NS = "{http://www.forensicswiki.org/wiki/Category:Digital_Forensics_XML}"
 
 def check_if_file_exists(path):
     if not os.path.isfile(path):
@@ -35,3 +36,50 @@ def get_fs_type(dd_image_path):
                     return (SupportedTypes.NTFS, lines)
                 
     return (SupportedTypes.UNSUPPORTED, None)
+
+def fiwalk(dd_image_path):
+    files = []
+    try:
+        _, output, _ = run_command("fiwalk -x -I -f " + dd_image_path)
+        root = ElementTree.fromstring(output)
+        # Since we will run fiwalk on a single FS partition, 
+        # we should only have one volume 
+        volume = root.find(FIWALK_DEFAULT_NS + "volume")
+        for fileobject in volume.findall(FIWALK_DEFAULT_NS + "fileobject"):
+            name_type = fileobject.find(FIWALK_DEFAULT_NS + "name_type")
+            inode = fileobject.find(FIWALK_DEFAULT_NS + "inode")
+            
+            # Let's analyse only regular files
+            if name_type == None or name_type.text != "r" or inode == None:
+                continue
+            file = {}
+            filename = fileobject.find(FIWALK_DEFAULT_NS + "filename")
+            libmagic = fileobject.find(FIWALK_DEFAULT_NS + "libmagic")
+            mtime = fileobject.find(FIWALK_DEFAULT_NS + "mtime")
+            ctime = fileobject.find(FIWALK_DEFAULT_NS + "ctime")
+            atime = fileobject.find(FIWALK_DEFAULT_NS + "atime")
+            crtime = fileobject.find(FIWALK_DEFAULT_NS + "crtime")
+
+            file["inode"] = inode.text if inode != None else None
+            file["filename"] = filename.text if filename != None else None
+            file["type"] = libmagic.text if libmagic != None else None
+            file["mtime"] = mtime.text if mtime != None else None
+            file["ctime"] = ctime.text if ctime != None else None
+            file["atime"] = atime.text if atime != None else None
+            file["crtime"] = crtime.text if crtime != None else None
+            file["md5"] =  None
+            file["sha1"] = None
+            for hashdigest in fileobject.findall(FIWALK_DEFAULT_NS + "hashdigest"):
+                if hashdigest.get("type") == "md5":
+                    file["md5"] = hashdigest.text
+                if hashdigest.get("type") == "sha1":
+                    file["sha1"] = hashdigest.text
+
+            files.append(file)
+    except:
+        print("Failed to execute fiwalk")
+    return files
+
+# files = fiwalk("./ntfs.dd")
+# for file in files:
+#     print(file)
